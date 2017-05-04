9bbe2a806840b9a63dd0703db81eff7f5ab04c9d
Vicky Huang <vicky@vickyhuang.com>
2017-05-04 00:45:03 -0700

    tweaks

--- en/EnviroCouture_files/EnviroCouture.js
+++ en/EnviroCouture_files/EnviroCouture.js
@@ -23,7 +23,7 @@ function onPageLoad()
 IWRegisterNamedImage('movie overlay','Media/Photo-Overlay-Movie.png')
 loadMozillaCSS('EnviroCouture_files/EnviroCoutureMoz.css')
 adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');NotificationCenter.addObserver(null,relayoutMediaGrid_id2,'RangeChanged','id2')
-Widget.onload();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id3');initializeMediaStream_id2()
+Widget.onload();fixAllIEPNGs('Media/transparent.gif');initializeMediaStream_id2()
 performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
